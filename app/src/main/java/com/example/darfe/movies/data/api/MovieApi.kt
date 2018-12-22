0ae87ff0f7cc2f19392aab5b60363a770e8d5c73
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-12-21 20:26:43 -0500

    URL & API added

--- app/src/main/java/com/example/darfe/movies/data/api/MovieApi.kt
+++ app/src/main/java/com/example/darfe/movies/data/api/MovieApi.kt
@@ -1,2 +1,18 @@
 package com.example.darfe.movies.data.api
 
+import com.example.darfe.movies.data.models.Movie
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApi {
+
+    @GET("/movie/now_playing")
+    fun getMovies(
+        @Query("api_key") apiKey: String,
+        @Query("language") l: String
+    ):Deferred<Results>
+
+}
+
+class Results(val results:List<Movie>)
